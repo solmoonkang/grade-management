54faa9b58bb791d0788d64e336aeaf0993d8c297
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-24 17:02:29 +0900

    feat(Student): Implemented a validation method in the Student class for the Status field

--- src/model/Student.java
+++ src/model/Student.java
@@ -8,17 +8,21 @@ public class Student {
     private final String name;
     private final int grade;
     private final Major major;
-    private final List<Course> courses;
+    private final Status status;
+    private List<Course> courses;
 
     public Student(Long id,
                    String name,
                    int grade,
                    Major major,
+                   Status status,
                    List<Course> courses) {
         this.id = id;
         this.name = name;
         this.grade = grade;
+        // TODO: Major 필드에 대한 유효성 검사도 추가할 수 있습니다. 메서드가 잘못된 문자열을 입력받는 경우 어떻게 처리할지 고려해보자.
         this.major = major;
+        this.status = validateStatus(status);
         this.courses = courses;
     }
 
@@ -30,6 +34,7 @@ public class Student {
         return this.id.equals(id);
     }
 
+    // TODO: hasSameName, hasSameDepartment, hasSameGrade 메서드는 equals 메서드를 오버라이드하여 하나의 메서드로 합칠 수 있다.
     public boolean hasSameName(String name) {
         return this.name.equals(name);
     }
@@ -40,5 +45,12 @@ public class Student {
 
     public boolean hasSameGrade(int grade) {
         return this.grade == grade;
+    }
+
+    private Status validateStatus(Status status) {
+        if (!status.isEnrolled() && !status.isAttending() && !status.isLeaveOfAbsence()) {
+            throw new IllegalArgumentException("The status must be either 'ENROLLED', 'ATTENDING', or 'LEAVEOFABSENCE'.");
+        }
+        return status;
     }
 }
