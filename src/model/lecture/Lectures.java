50a9b097cbbccc19c58ae753035a30e54cb70853
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-12-28 03:37:49 +0900

    feat(Lectures): Implement method to remove lecture in Lectures class

--- src/model/lecture/Lectures.java
+++ src/model/lecture/Lectures.java
@@ -48,12 +48,22 @@ public class Lectures {
         lecture.updateLecture(newLecture);
     }
 
+    public void removeLecture(String name) {
+        Lecture lecture = lectures.stream()
+                .filter(existingLecture -> existingLecture.hasSameName(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] Lecture not found with name"));
+
+        lectures.remove(lecture);
+    }
+
     private void isDuplicatedName(Lecture newLecture) {
         if (lectures.stream().anyMatch(lecture -> lecture.hasSameName(newLecture))) {
             throw new IllegalArgumentException("[ERROR] Lecture Name is Duplicated.");
         }
     }
 
+    // TODO: 중복되는 로직들에 대한 정리가 필요
     private void isDuplicatedNameExceptSelf(Lecture existLecture, Lecture newLecture) {
         if (lectures.stream().anyMatch(lecture -> !lecture.equals(existLecture) && lecture.hasSameName(newLecture))) {
             throw new IllegalArgumentException("[ERROR] Lecture Name is Duplicated.");
