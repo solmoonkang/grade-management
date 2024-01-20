0c04406cf7446ca1e8fd5558ff2bfa6cdc4b4b20
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-20 12:41:04 +0900

    feat: Implement user input student info

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -12,4 +12,9 @@ public class InputView {
         System.out.println("1: 학생 추가, 2: 학생 삭제, 3: 학생 검색, 4: 성적 평균 확인");
         return reader.readLine();
     }
+
+    public String inputStudentInfo() throws IOException {
+        System.out.println("추가하고 싶은 학생의 이름, 학번, 성별을 입력해주세요.");
+        return reader.readLine();
+    }
 }
