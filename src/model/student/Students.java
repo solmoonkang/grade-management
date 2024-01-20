af7c239d2eeaf229b23798659f65684b5891cf99
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-20 14:01:02 +0900

    refactor: Refactor Students class to use streams

--- src/model/student/Students.java
+++ src/model/student/Students.java
@@ -1,7 +1,9 @@
 package model.student;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Students {
 
@@ -15,21 +17,19 @@ public class Students {
     }
 
     public void parseMultipleStudentInfo(String studentInfos) {
-        String[] individualInfo = studentInfos.split(COMMA);
-
-        for (String info : individualInfo) {
-            parseStudentInfo(info.trim());
-        }
+        students.addAll(Arrays.stream(studentInfos.split(COMMA))
+                .map(String::trim)
+                .map(this::parseStudentInfo)
+                .toList());
     }
 
-    private void parseStudentInfo(String studentInfo) {
+    private Student parseStudentInfo(String studentInfo) {
         String[] infos = studentInfo.split(SPACE);
         Long id = Long.parseLong(infos[0]);
         String name = infos[1];
         String gender = infos[2];
         Grade grade = Grade.valueOf(infos[3].toUpperCase());
 
-        Student student = new Student(id, name, gender, grade);
-        students.add(student);
+        return new Student(id, name, gender, grade);
     }
 }
