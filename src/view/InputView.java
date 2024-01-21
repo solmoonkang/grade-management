f54e7caf4a600f0d8e0b05ec1a224cf9cdf8d815
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-21 17:25:24 +0900

    feat: Implement inputStudentGenderToAdd method in InputView class

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -23,6 +23,11 @@ public class InputView {
         return reader.readLine();
     }
 
+    public String inputStudentGenderToAdd() throws IOException {
+        System.out.println("추가하고 싶은 학생의 성별을 입력해주세요.");
+        return reader.readLine();
+    }
+
     public String inputStudentIdToRemove() throws IOException {
         System.out.println("삭제하고 싶은 학생의 학번을 입력해주세요.");
         return reader.readLine();
